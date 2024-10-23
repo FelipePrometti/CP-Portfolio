e781ce4f82a4d5b20fce81a6e4b21275c905403e
samueldamasceno <samueldamasceno.vip@gmail.com>
2024-10-23 12:08:01 -0300

    atualização das paginas das rotas

--- cp-portfolio/src/app/checkpoints/page.tsx
+++ cp-portfolio/src/app/checkpoints/page.tsx
@@ -2,7 +2,6 @@ export default function CheckPoints() {
     return (
       <section className="container mx-auto py-10">
         <h1 className="text-3xl font-bold">CheckPoints</h1>
-        <p className="mt-4">Aqui estão listados todos os seus checkpoints.</p>
       </section>
     );
   }
